eed9b175695e246b7a990d661e0155ec20dd3942
ofurum <chiagoziem.ofurum@thebulb.africa>
2021-08-20 01:10:42 +0100

    initial commit

---
+++ src/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss'
+
+const HompePage = () => {
+ 
+    return(
+        <div className="home">
+            
+        </div>
+    )
+}
+
+export default HompePage;
